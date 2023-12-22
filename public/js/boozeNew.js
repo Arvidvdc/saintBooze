0936a7236b8365a7bd116cd5d9275652d14cc9e6
Arvidvdc <arvidvdc@gmail.com>
2023-12-22 22:53:00 +0100

    Layout adding record completed.

--- public/js/boozeNew.js
+++ public/js/boozeNew.js
@@ -1,1 +1,30 @@
 console.log("boozeNew reporting");
+
+openSite = (givenSite) => {
+
+    let gotoWebsite, url;
+    gotoWebsite = document.getElementById(givenSite).value;
+    if(gotoWebsite.length>0) {
+        url = gotoWebsite
+        window.open(url, "_blank");
+    } else {
+        alert("Vul eerst een website in!")
+    }
+}
+
+enterImage = () => {
+    let url,  isNum, target;
+    target = document.getElementById("image");
+
+    if(target.value.length>0) {
+        window.open(target.value, "_blank");
+    } else {
+        url = prompt("Plak de url naar de afbeelding");
+        isNum = !isNaN(Number(url));
+        if(!isNum) {
+            target.value=url;
+        } else {
+            alert("Geen URL opgegevn!");
+        }
+    }
+}
