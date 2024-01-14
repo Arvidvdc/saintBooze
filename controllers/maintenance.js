68291df406246bf413d21db1bbcbddb4ce687469
Arvidvdc <arvidvdc@gmail.com>
2024-01-14 01:52:49 +0100

    Maintenance create done.

--- controllers/maintenance.js
+++ controllers/maintenance.js
@@ -1,4 +1,5 @@
 // Required dependencies
+const maintenance = require("../models/Maintenance");
 const   MAINTENANCE  = require("../models/Maintenance");
 
 // Index controller
@@ -8,7 +9,21 @@ exports.index = (req,res) => {
 
 // Create controller
 exports.add = (req,res) => {
-    res.send("MAINTENANCE CREATE");
+    let newMaintenance = {
+        item        : req.body.item,
+        category    : req.body.category,
+        description : req.body.description,
+        isActive    : true
+    }
+    MAINTENANCE.create(newMaintenance)
+    .then((createdMaintenance) => {
+        console.log("Maintenance created: ", createdMaintenance);
+        res.redirect("/maintenance");
+    })
+    .catch((error) => {
+        console.log("Error creating maintenance:", error);
+        res.send("Fout bij het opslaan van: " + req.body.item + "\n" + error);
+    });
 }
 
 // New controller
